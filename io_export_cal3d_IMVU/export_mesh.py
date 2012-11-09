88d3e3c009e440af5497e0932df001bd4cbab72e
Jacob Boerema <jgboerema@gmail.com>
2012-11-09 13:55:36 +0100

    Add short project description to all files.

--- io_export_cal3d_IMVU/export_mesh.py
+++ io_export_cal3d_IMVU/export_mesh.py
@@ -1,5 +1,8 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
+# This file is part of the Blender 2.63+ to Cal3d exporter targeted
+# primarily for IMVU compatibility.
+#
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 2
