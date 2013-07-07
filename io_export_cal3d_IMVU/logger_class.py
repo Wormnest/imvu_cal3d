b90aa22479b2d6323da7236a9c60e369ae8a2dc0
Jacob Boerema <jgboerema@gmail.com>
2013-07-07 16:51:43 +0200

    Feature: start working on a logging class.

---
+++ io_export_cal3d_IMVU/logger_class.py
@@ -0,0 +1,40 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+# This file is part of the Blender 2.63+ to Cal3d exporter targeted
+# primarily for IMVU compatibility.
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# A class to log messages
+
+class Logger:
+
+    def __init__(self, name):
+        self.name = name
+        self.errors = 0
+        self.warnings = 0
+        self.debug = 0
+        self.info = 0
+
+    def log_warning(self, warning):
+        self.warnings += 1
+        print(warning)
+
+# ---------------------------------------------------------------------------------------------------------------------------------
+
+# Current logger class:
+LogMessage = None
