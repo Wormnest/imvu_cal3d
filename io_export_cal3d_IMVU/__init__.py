fa3cdd2cb5e4885f16f3ee5f4cd738b6a14d9c9a
Jacob Boerema <jgboerema@gmail.com>
2013-07-07 14:19:15 +0200

    Update to version 1.4.1 since we just released version 1.4.

--- io_export_cal3d_IMVU/__init__.py
+++ io_export_cal3d_IMVU/__init__.py
@@ -31,7 +31,7 @@ bl_info = \
             "Peter Amstutz, "             \
             "Etory, "                     \
             "Jacob Boerema",
-  "version": (1, 4, 0),
+  "version": (1, 4, 1),
   "blender": (2, 6, 3),
   "location": "File > Export > IMVU Cal3D (.cfg)",
   "description": "Export mesh geometry, armature, "   \
