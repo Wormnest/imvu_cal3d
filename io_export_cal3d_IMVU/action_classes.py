983d9cbb84a1115d20dc62d1e335864142974f1e
Jacob Boerema <jgboerema@gmail.com>
2013-07-07 15:15:00 +0200

    Change: remove all tabs from action_classes.py and export_action.py.

--- io_export_cal3d_IMVU/action_classes.py
+++ io_export_cal3d_IMVU/action_classes.py
@@ -27,112 +27,112 @@ import bpy
 from mathutils import *
 
 class KeyFrame:
-	def __init__(self, time, loc, quat):
-		self.time = time
-		self.loc = loc.copy()
-		self.quat = quat.copy()
+    def __init__(self, time, loc, quat):
+        self.time = time
+        self.loc = loc.copy()
+        self.quat = quat.copy()
  
-	def to_cal3d_xml(self):
-		s = "    <KEYFRAME TIME=\"{0:0.5f}\">\n".format(self.time)
-		s += "      <TRANSLATION>{0:0.6f} {1:0.6f} {2:0.6f}</TRANSLATION>\n".format(self.loc[0], self.loc[1], self.loc[2])
+    def to_cal3d_xml(self):
+        s = "    <KEYFRAME TIME=\"{0:0.5f}\">\n".format(self.time)
+        s += "      <TRANSLATION>{0:0.6f} {1:0.6f} {2:0.6f}</TRANSLATION>\n".format(self.loc[0], self.loc[1], self.loc[2])
 
-		# jgb 2012-11-11 Maybe we need for w: -self.quat.w to get the same negative value as for the mesh.
-		s += "      <ROTATION>{0:0.6f} {1:0.6f} {2:0.6f} {3:0.6f}</ROTATION>\n".format(self.quat.copy().x, 
-		                                                           self.quat.copy().y, 
-		                                                           self.quat.copy().z, 
-		                                                           -self.quat.copy().w)
-		s += "    </KEYFRAME>\n"
-		return s
+        # jgb 2012-11-11 Maybe we need for w: -self.quat.w to get the same negative value as for the mesh.
+        s += "      <ROTATION>{0:0.6f} {1:0.6f} {2:0.6f} {3:0.6f}</ROTATION>\n".format(self.quat.copy().x, 
+                                                                   self.quat.copy().y, 
+                                                                   self.quat.copy().z, 
+                                                                   -self.quat.copy().w)
+        s += "    </KEYFRAME>\n"
+        return s
 
-		
-	def to_cal3d_binary(self, file):
-		ar = array('f', [self.time,
-						 self.loc[0],
-						 self.loc[1],
-						 self.loc[2],
-						 self.quat.copy().x, 
-						 self.quat.copy().y,
-						 self.quat.copy().z,
-						 -self.quat.copy().w])
-		ar.tofile(file)
+        
+    def to_cal3d_binary(self, file):
+        ar = array('f', [self.time,
+                         self.loc[0],
+                         self.loc[1],
+                         self.loc[2],
+                         self.quat.copy().x, 
+                         self.quat.copy().y,
+                         self.quat.copy().z,
+                         -self.quat.copy().w])
+        ar.tofile(file)
 
 
 
 class Track:
-	def __init__(self, bone_index):
-		self.bone_index = bone_index
-		self.keyframes = []
-		# jgb 2012-11-14 If I'm reading the Cal3d source correctly (saver.cpp, CalSaver::saveXmlCoreAnimation)
-		# Then TRANSLATIONREQUIRED=1 means there will be a TRANSLATION record for the first keyframe
-		# The next keyframes don't require a TRANSLATION record, unless TRANSLATIONISDYNAMIC=1, then every keyframe needs it.
-		# Therefore we set translationisdynamic to 1 here.
-		# Seems highrangerequired also defaults to true (see xmlformat.cpp, CalCoreAnimationPtr CalLoader::loadXmlCoreAnimation)
-		# highrangerequired seems to be used to compress animation, when it's off it uses some bits for other purposes, we leave it on for now
-		# TODO: We should make this dependent on whether or not we actually need to use the translation record or not.
-		self.translationrequired = 1
-		self.translationisdynamic = 1
-		self.highrangerequired = 1
+    def __init__(self, bone_index):
+        self.bone_index = bone_index
+        self.keyframes = []
+        # jgb 2012-11-14 If I'm reading the Cal3d source correctly (saver.cpp, CalSaver::saveXmlCoreAnimation)
+        # Then TRANSLATIONREQUIRED=1 means there will be a TRANSLATION record for the first keyframe
+        # The next keyframes don't require a TRANSLATION record, unless TRANSLATIONISDYNAMIC=1, then every keyframe needs it.
+        # Therefore we set translationisdynamic to 1 here.
+        # Seems highrangerequired also defaults to true (see xmlformat.cpp, CalCoreAnimationPtr CalLoader::loadXmlCoreAnimation)
+        # highrangerequired seems to be used to compress animation, when it's off it uses some bits for other purposes, we leave it on for now
+        # TODO: We should make this dependent on whether or not we actually need to use the translation record or not.
+        self.translationrequired = 1
+        self.translationisdynamic = 1
+        self.highrangerequired = 1
 
 
-	def to_cal3d_xml(self):
-		s = "  <TRACK BONEID=\"{0}\" TRANSLATIONREQUIRED=\"{1}\" TRANSLATIONISDYNAMIC=\"{2}\" ".format(self.bone_index, self.translationrequired, self.translationisdynamic)
-		s += "HIGHRANGEREQUIRED=\"{0}\" NUMKEYFRAMES=\"{1}\">\n".format(self.highrangerequired, len(self.keyframes))
-		s += "".join(map(KeyFrame.to_cal3d_xml, self.keyframes))
-		s += "  </TRACK>\n"
-		return s
+    def to_cal3d_xml(self):
+        s = "  <TRACK BONEID=\"{0}\" TRANSLATIONREQUIRED=\"{1}\" TRANSLATIONISDYNAMIC=\"{2}\" ".format(self.bone_index, self.translationrequired, self.translationisdynamic)
+        s += "HIGHRANGEREQUIRED=\"{0}\" NUMKEYFRAMES=\"{1}\">\n".format(self.highrangerequired, len(self.keyframes))
+        s += "".join(map(KeyFrame.to_cal3d_xml, self.keyframes))
+        s += "  </TRACK>\n"
+        return s
 
-		
-	def to_cal3d_binary(self, file):
-		ar = array('L', [self.bone_index,
-						 len(self.keyframes)])
-		ar.tofile(file)
-		
-		for kf in self.keyframes:
-			kf.to_cal3d_binary(file)
+        
+    def to_cal3d_binary(self, file):
+        ar = array('L', [self.bone_index,
+                         len(self.keyframes)])
+        ar.tofile(file)
+        
+        for kf in self.keyframes:
+            kf.to_cal3d_binary(file)
 
 
 
 class Animation:
-	def __init__(self, name, xml_version):
-		self.name = name
-		self.xml_version = xml_version
-		self.duration = 0.0
-		self.tracks = []
+    def __init__(self, name, xml_version):
+        self.name = name
+        self.xml_version = xml_version
+        self.duration = 0.0
+        self.tracks = []
 
 
-	def to_cal3d_xml(self):
-		s = "<HEADER MAGIC=\"XAF\" VERSION=\"{0}\"/>\n".format(self.xml_version)
-		s += "<ANIMATION DURATION=\"{0:0.5f}\" NUMTRACKS=\"{1}\">\n".format(self.duration, len(self.tracks))
-		s += "".join(map(Track.to_cal3d_xml, self.tracks))
-		s += "</ANIMATION>\n"
-		return s
+    def to_cal3d_xml(self):
+        s = "<HEADER MAGIC=\"XAF\" VERSION=\"{0}\"/>\n".format(self.xml_version)
+        s += "<ANIMATION DURATION=\"{0:0.5f}\" NUMTRACKS=\"{1}\">\n".format(self.duration, len(self.tracks))
+        s += "".join(map(Track.to_cal3d_xml, self.tracks))
+        s += "</ANIMATION>\n"
+        return s
 
-		
-	def to_cal3d_binary(self, file):
-		s = b'CAF\0'
-		ar = array('b', list(s))
-		ar.tofile(file)
-		
-		# Etory : downgrade version to 700 for Cal3D 0.11 compatibility
-		ar = array('L', [700])
-		ar.tofile(file)
-		
-		ar = array('L', [0]) # this is an unknown value that has to be there
-		ar.tofile(file)
-		
-		ar = array('f', [self.duration])
-		ar.tofile(file)
-		
-		ar = array('L', [len(self.tracks),
-						 0]) # flags for tracks      Bit 0: 1 if compressed tracks
-		ar.tofile(file)
-		
-		if ar[1] == 0: # normal uncompressed tracks
-			for tr in self.tracks:
-				tr.to_cal3d_binary(file)
-		else: # compressed tracks
-			for tr in self.tracks: # not sure what to do here yet
-				tr.to_cal3d_binary(file)
+        
+    def to_cal3d_binary(self, file):
+        s = b'CAF\0'
+        ar = array('b', list(s))
+        ar.tofile(file)
+        
+        # Etory : downgrade version to 700 for Cal3D 0.11 compatibility
+        ar = array('L', [700])
+        ar.tofile(file)
+        
+        ar = array('L', [0]) # this is an unknown value that has to be there
+        ar.tofile(file)
+        
+        ar = array('f', [self.duration])
+        ar.tofile(file)
+        
+        ar = array('L', [len(self.tracks),
+                         0]) # flags for tracks      Bit 0: 1 if compressed tracks
+        ar.tofile(file)
+        
+        if ar[1] == 0: # normal uncompressed tracks
+            for tr in self.tracks:
+                tr.to_cal3d_binary(file)
+        else: # compressed tracks
+            for tr in self.tracks: # not sure what to do here yet
+                tr.to_cal3d_binary(file)
 
 
 # ====================================
@@ -141,43 +141,43 @@ class Animation:
 
 # Class MorphKeyFrame store a keyframe of a morph animation and allows export to XML only.
 class MorphKeyFrame:
-	def __init__(self, time, weight):
-		self.time = time
-		self.weight = weight
+    def __init__(self, time, weight):
+        self.time = time
+        self.weight = weight
  
-	def to_cal3d_xml(self):
-		s = "    <KEYFRAME TIME=\"{0:0.5f}\">\n".format(self.time)
-		s += "      <WEIGHT>{0:0.6f}</WEIGHT>\n".format(self.weight)
-		s += "    </KEYFRAME>\n"
-		return s
+    def to_cal3d_xml(self):
+        s = "    <KEYFRAME TIME=\"{0:0.5f}\">\n".format(self.time)
+        s += "      <WEIGHT>{0:0.6f}</WEIGHT>\n".format(self.weight)
+        s += "    </KEYFRAME>\n"
+        return s
 
 
 # Class MorphTrack stores Morph Animation tracks and allows XML export only.
 class MorphTrack:
-	def __init__(self, morph_name):
-		self.morph_name = morph_name
-		self.keyframes = []
+    def __init__(self, morph_name):
+        self.morph_name = morph_name
+        self.keyframes = []
 
 
-	def to_cal3d_xml(self):
-		s = "  <TRACK NUMKEYFRAMES=\"{0}\" MORPHNAME=\"{1}\">\n".format(len(self.keyframes), self.morph_name)
-		s += "".join(map(MorphKeyFrame.to_cal3d_xml, self.keyframes))
-		s += "  </TRACK>\n"
-		return s
+    def to_cal3d_xml(self):
+        s = "  <TRACK NUMKEYFRAMES=\"{0}\" MORPHNAME=\"{1}\">\n".format(len(self.keyframes), self.morph_name)
+        s += "".join(map(MorphKeyFrame.to_cal3d_xml, self.keyframes))
+        s += "  </TRACK>\n"
+        return s
 
 
 # Class MorphAnimation stores morph animation data and allows XML export only.
 class MorphAnimation:
-	def __init__(self, name, xml_version):
-		self.name = name
-		self.xml_version = xml_version
-		self.duration = 0.0
-		self.morph_tracks = []
+    def __init__(self, name, xml_version):
+        self.name = name
+        self.xml_version = xml_version
+        self.duration = 0.0
+        self.morph_tracks = []
 
 
-	def to_cal3d_xml(self):
-		s = "<HEADER MAGIC=\"XPF\" VERSION=\"{0}\"/>\n".format(self.xml_version)
-		s += "<ANIMATION NUMTRACKS=\"{0}\" DURATION=\"{1:0.5f}\">\n".format(len(self.morph_tracks), self.duration)
-		s += "".join(map(MorphTrack.to_cal3d_xml, self.morph_tracks))
-		s += "</ANIMATION>\n"
-		return s
+    def to_cal3d_xml(self):
+        s = "<HEADER MAGIC=\"XPF\" VERSION=\"{0}\"/>\n".format(self.xml_version)
+        s += "<ANIMATION NUMTRACKS=\"{0}\" DURATION=\"{1:0.5f}\">\n".format(len(self.morph_tracks), self.duration)
+        s += "".join(map(MorphTrack.to_cal3d_xml, self.morph_tracks))
+        s += "</ANIMATION>\n"
+        return s
--- io_export_cal3d_IMVU/export_action.py
+++ io_export_cal3d_IMVU/export_action.py
@@ -29,215 +29,213 @@ from . import action_classes
 from .action_classes import *
 
 def get_action_group_fcurve(action_group, data_path, array_index):
-	for fcu in action_group.channels:
-		if fcu.data_path.find(data_path) != -1 and \
-		    fcu.array_index == array_index:
-			return fcu
+    for fcu in action_group.channels:
+        if fcu.data_path.find(data_path) != -1 and \
+            fcu.array_index == array_index:
+            return fcu
 
-	return None
+    return None
 
 
 def get_keyframes_list(fcu):
-	keyframes_list = []
-	if fcu:
-		for keyframe in fcu.keyframe_points:
-			keyframes_list.append(keyframe.co[0])
-	return keyframes_list
+    keyframes_list = []
+    if fcu:
+        for keyframe in fcu.keyframe_points:
+            keyframes_list.append(keyframe.co[0])
+    return keyframes_list
 
 
 def evaluate_loc(loc_x_fcu, loc_y_fcu, loc_z_fcu, keyframe):
-	loc_x = 0.0
-	loc_y = 0.0
-	loc_z = 0.0
+    loc_x = 0.0
+    loc_y = 0.0
+    loc_z = 0.0
 
-	if loc_x_fcu:
-		loc_x = loc_x_fcu.evaluate(keyframe)
+    if loc_x_fcu:
+        loc_x = loc_x_fcu.evaluate(keyframe)
 
-	if loc_y_fcu:
-		loc_y = loc_y_fcu.evaluate(keyframe)
+    if loc_y_fcu:
+        loc_y = loc_y_fcu.evaluate(keyframe)
 
-	if loc_z_fcu:
-		loc_z = loc_z_fcu.evaluate(keyframe)
+    if loc_z_fcu:
+        loc_z = loc_z_fcu.evaluate(keyframe)
 
-	return mathutils.Vector([loc_x, loc_y, loc_z])
+    return mathutils.Vector([loc_x, loc_y, loc_z])
 
 
 def evaluate_quat(quat_x_fcu, quat_y_fcu, quat_z_fcu, quat_w_fcu, keyframe):
-	quat_x = 0.0
-	quat_y = 0.0
-	quat_z = 0.0
-	# jgb 2012-11-11 Blender has a w value of 1.0 when we haven't changed the rotation so try that instead of 0.0
-	quat_w = 1.0
+    quat_x = 0.0
+    quat_y = 0.0
+    quat_z = 0.0
+    # jgb 2012-11-11 Blender has a w value of 1.0 when we haven't changed the rotation so try that instead of 0.0
+    quat_w = 1.0
 
-	if quat_x_fcu:
-		quat_x = quat_x_fcu.evaluate(keyframe)
+    if quat_x_fcu:
+        quat_x = quat_x_fcu.evaluate(keyframe)
 
-	if quat_y_fcu:
-		quat_y = quat_y_fcu.evaluate(keyframe)
+    if quat_y_fcu:
+        quat_y = quat_y_fcu.evaluate(keyframe)
 
-	if quat_z_fcu:
-		quat_z = quat_z_fcu.evaluate(keyframe)
+    if quat_z_fcu:
+        quat_z = quat_z_fcu.evaluate(keyframe)
 
-	if quat_w_fcu:
-		quat_w = quat_w_fcu.evaluate(keyframe)
+    if quat_w_fcu:
+        quat_w = quat_w_fcu.evaluate(keyframe)
 
-	# jgb 2012-11-11 I think with quaternions w needs to come first, not last.
-	return mathutils.Quaternion([quat_w, quat_x, quat_y, quat_z])
+    # jgb 2012-11-11 I think with quaternions w needs to come first, not last.
+    return mathutils.Quaternion([quat_w, quat_x, quat_y, quat_z])
 
 
 def track_sort_key(track):
-	return track.bone_index
+    return track.bone_index
 
 
 def create_cal3d_animation(cal3d_skeleton, action, fps, xml_version):
-	cal3d_animation = Animation(action.name, xml_version)
+    cal3d_animation = Animation(action.name, xml_version)
 
-	initialized_borders = False
-	last_keyframe = 0
-	first_keyframe = 0
+    initialized_borders = False
+    last_keyframe = 0
+    first_keyframe = 0
 
-	for action_group in action.groups:
-		cal3d_bone = None
+    for action_group in action.groups:
+        cal3d_bone = None
 
-		for bone in cal3d_skeleton.bones:
-			if bone.name == action_group.name:
-				cal3d_bone = bone
-				break
+        for bone in cal3d_skeleton.bones:
+            if bone.name == action_group.name:
+                cal3d_bone = bone
+                break
 
-		if not cal3d_bone:
-			print("WARNING: no bone found corresponding to action group "+action_group.name)
-			continue
+        if not cal3d_bone:
+            print("WARNING: no bone found corresponding to action group "+action_group.name)
+            continue
 
-		cal3d_track = Track(cal3d_bone.index)
+        cal3d_track = Track(cal3d_bone.index)
 
-		loc_x_fcu = get_action_group_fcurve(action_group, "location", 0)
-		loc_y_fcu = get_action_group_fcurve(action_group, "location", 1)
-		loc_z_fcu = get_action_group_fcurve(action_group, "location", 2)
+        loc_x_fcu = get_action_group_fcurve(action_group, "location", 0)
+        loc_y_fcu = get_action_group_fcurve(action_group, "location", 1)
+        loc_z_fcu = get_action_group_fcurve(action_group, "location", 2)
 
-		# jgb NB: w first instead of last, thus has index 0, not 3!
-		quat_w_fcu = get_action_group_fcurve(action_group,
-				                             "rotation_quaternion", 0)
-		quat_x_fcu = get_action_group_fcurve(action_group, 
-				                             "rotation_quaternion", 1)
-		quat_y_fcu = get_action_group_fcurve(action_group,
-				                             "rotation_quaternion", 2)
-		quat_z_fcu = get_action_group_fcurve(action_group,
-				                             "rotation_quaternion", 3)
+        # jgb NB: w first instead of last, thus has index 0, not 3!
+        quat_w_fcu = get_action_group_fcurve(action_group,
+                                             "rotation_quaternion", 0)
+        quat_x_fcu = get_action_group_fcurve(action_group, 
+                                             "rotation_quaternion", 1)
+        quat_y_fcu = get_action_group_fcurve(action_group,
+                                             "rotation_quaternion", 2)
+        quat_z_fcu = get_action_group_fcurve(action_group,
+                                             "rotation_quaternion", 3)
 
-		keyframes_list = []
+        keyframes_list = []
 
-		keyframes_list.extend(get_keyframes_list(loc_x_fcu))
-		keyframes_list.extend(get_keyframes_list(loc_y_fcu))
-		keyframes_list.extend(get_keyframes_list(loc_z_fcu))
+        keyframes_list.extend(get_keyframes_list(loc_x_fcu))
+        keyframes_list.extend(get_keyframes_list(loc_y_fcu))
+        keyframes_list.extend(get_keyframes_list(loc_z_fcu))
 
-		keyframes_list.extend(get_keyframes_list(quat_x_fcu))
-		keyframes_list.extend(get_keyframes_list(quat_y_fcu))
-		keyframes_list.extend(get_keyframes_list(quat_z_fcu))
-		keyframes_list.extend(get_keyframes_list(quat_w_fcu))
+        keyframes_list.extend(get_keyframes_list(quat_x_fcu))
+        keyframes_list.extend(get_keyframes_list(quat_y_fcu))
+        keyframes_list.extend(get_keyframes_list(quat_z_fcu))
+        keyframes_list.extend(get_keyframes_list(quat_w_fcu))
 
-		# remove duplicates
-		keyframes_set = set(keyframes_list)
-		keyframes_list = list(keyframes_set)
-		keyframes_list.sort()
-		
-		if len(keyframes_list) == 0:
-			print("WARNING: no keyframes in action group "+action_group.name)
-			continue
+        # remove duplicates
+        keyframes_set = set(keyframes_list)
+        keyframes_list = list(keyframes_set)
+        keyframes_list.sort()
+        
+        if len(keyframes_list) == 0:
+            print("WARNING: no keyframes in action group "+action_group.name)
+            continue
 
-		if initialized_borders:
-			first_keyframe = min(keyframes_list[0], first_keyframe)
-			last_keyframe = max(keyframes_list[len(keyframes_list) - 1], 
-			                    last_keyframe)
-		else:
-			first_keyframe = keyframes_list[0]
-			last_keyframe = keyframes_list[len(keyframes_list) - 1]
-			initialized_borders = True
+        if initialized_borders:
+            first_keyframe = min(keyframes_list[0], first_keyframe)
+            last_keyframe = max(keyframes_list[len(keyframes_list) - 1], 
+                                last_keyframe)
+        else:
+            first_keyframe = keyframes_list[0]
+            last_keyframe = keyframes_list[len(keyframes_list) - 1]
+            initialized_borders = True
 
-		cal3d_track.keyframes = []
+        cal3d_track.keyframes = []
 
-		for keyframe in keyframes_list:
-			dloc = evaluate_loc(loc_x_fcu, loc_y_fcu, loc_z_fcu, keyframe)
-			dquat = evaluate_quat(quat_x_fcu, quat_y_fcu, 
-			                      quat_z_fcu, quat_w_fcu, keyframe)
+        for keyframe in keyframes_list:
+            dloc = evaluate_loc(loc_x_fcu, loc_y_fcu, loc_z_fcu, keyframe)
+            dquat = evaluate_quat(quat_x_fcu, quat_y_fcu, 
+                                  quat_z_fcu, quat_w_fcu, keyframe)
 
-			quat = dquat.copy()
-			quat.rotate(cal3d_bone.quat)
-			quat.normalize()
+            quat = dquat.copy()
+            quat.rotate(cal3d_bone.quat)
+            quat.normalize()
 
-			dloc.x *= cal3d_skeleton.anim_scale.x
-			dloc.y *= cal3d_skeleton.anim_scale.y
-			dloc.z *= cal3d_skeleton.anim_scale.z
+            dloc.x *= cal3d_skeleton.anim_scale.x
+            dloc.y *= cal3d_skeleton.anim_scale.y
+            dloc.z *= cal3d_skeleton.anim_scale.z
 
-			dloc.rotate(cal3d_bone.quat)
-			loc = cal3d_bone.loc + dloc
+            dloc.rotate(cal3d_bone.quat)
+            loc = cal3d_bone.loc + dloc
 
-			cal3d_keyframe = KeyFrame(keyframe, loc, quat)
-			cal3d_track.keyframes.append(cal3d_keyframe)
+            cal3d_keyframe = KeyFrame(keyframe, loc, quat)
+            cal3d_track.keyframes.append(cal3d_keyframe)
 
-		if len(cal3d_track.keyframes) > 0:
-			cal3d_animation.tracks.append(cal3d_track)
+        if len(cal3d_track.keyframes) > 0:
+            cal3d_animation.tracks.append(cal3d_track)
 
-	cal3d_animation.duration = ((last_keyframe - first_keyframe) / fps)
-	cal3d_animation.tracks.sort(key=track_sort_key)
+    cal3d_animation.duration = ((last_keyframe - first_keyframe) / fps)
+    cal3d_animation.tracks.sort(key=track_sort_key)
 
-	for track in cal3d_animation.tracks:
-		for keyframe in track.keyframes:
-			keyframe.time = (keyframe.time - first_keyframe) / fps
+    for track in cal3d_animation.tracks:
+        for keyframe in track.keyframes:
+            keyframe.time = (keyframe.time - first_keyframe) / fps
 
 
-	if len(cal3d_animation.tracks) > 0:
-		return cal3d_animation
+    if len(cal3d_animation.tracks) > 0:
+        return cal3d_animation
 
-	return None
+    return None
 
 def MorphFromDataPath(dataPath):
-	if dataPath.startswith("key_blocks["):
-		words = dataPath.split('"')
-		if len(words) == 3:
-			return words[1]
-		else:
-			print("UNEXPECTED datapath type!")
-			#  e.g. location
-			return None
-	else:
-		print("UNEXPECTED datapath type!")
-		return None
+    if dataPath.startswith("key_blocks["):
+        words = dataPath.split('"')
+        if len(words) == 3:
+            return words[1]
+        else:
+            print("UNEXPECTED datapath type!")
+            #  e.g. location
+            return None
+    else:
+        print("UNEXPECTED datapath type!")
+        return None
 
 # jgb: Morph animation export handler based on the normal animation handler
 # Note: the shape_keys parameter is currently not used but as I'm not sure whether I won't need it
 # in the future here I'm leaving it in
 def create_cal3d_morph_animation(shape_keys, action, fps, xml_version):
-	cal3d_morph_animation = MorphAnimation(action.name, xml_version)
-	print("Morph animation: "+action.name)
-	# determine animation duration
-	cal3d_morph_animation.duration = ((action.frame_range.y - action.frame_range.x) / fps)
+    cal3d_morph_animation = MorphAnimation(action.name, xml_version)
+    print("Morph animation: "+action.name)
+    # determine animation duration
+    cal3d_morph_animation.duration = ((action.frame_range.y - action.frame_range.x) / fps)
 
-	# loop over  all curves in this action
-	for fcu in action.fcurves:
-		# Decipher morph name  from datapath
-		morph_name = MorphFromDataPath(fcu.data_path)
-		if morph_name:
-			# Add a track with this morphname
-			cal3d_morph_track = MorphTrack(morph_name)
-			if cal3d_morph_track:
-				# Add track to morph animation
-				cal3d_morph_animation.morph_tracks.append(cal3d_morph_track)
-				#print("Track for morph name:"+morph_name)
-				if len(fcu.keyframe_points) > 0:
-					# Keyframes present
-					for key in fcu.keyframe_points:
-						# value = weight in this context
-						frame, value = key.co
-						# Compute KeyFrame time from frame and framerate
-						frame_time = frame / fps
-						# Add KeyFrame for morph
-						cal3d_morph_key_frame = MorphKeyFrame(frame_time,value)
-						if cal3d_morph_key_frame:
-							#print("frame, weight: "+ str(frame)+", "+str(value))
-							cal3d_morph_track.keyframes.append(cal3d_morph_key_frame)
-				else:
-					print("WARNING: no keyframe points for morph "+morph_name)
+    # loop over  all curves in this action
+    for fcu in action.fcurves:
+        # Decipher morph name  from datapath
+        morph_name = MorphFromDataPath(fcu.data_path)
+        if morph_name:
+            # Add a track with this morphname
+            cal3d_morph_track = MorphTrack(morph_name)
+            if cal3d_morph_track:
+                # Add track to morph animation
+                cal3d_morph_animation.morph_tracks.append(cal3d_morph_track)
+                #print("Track for morph name:"+morph_name)
+                if len(fcu.keyframe_points) > 0:
+                    # Keyframes present
+                    for key in fcu.keyframe_points:
+                        # value = weight in this context
+                        frame, value = key.co
+                        # Compute KeyFrame time from frame and framerate
+                        frame_time = frame / fps
+                        # Add KeyFrame for morph
+                        cal3d_morph_key_frame = MorphKeyFrame(frame_time,value)
+                        if cal3d_morph_key_frame:
+                            #print("frame, weight: "+ str(frame)+", "+str(value))
+                            cal3d_morph_track.keyframes.append(cal3d_morph_key_frame)
+                else:
+                    print("WARNING: no keyframe points for morph "+morph_name)
 
-	return cal3d_morph_animation
-
-
+    return cal3d_morph_animation
